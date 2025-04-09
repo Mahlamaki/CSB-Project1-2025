dbd6447e8158fbe4811ee8760028b7dd3dc7cf97
Mahlamaki <liisa.mahlamaki@helsinki.fi>
2025-04-09 23:30:33 +0300

    SQL injection flaw

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,8 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.db import connection
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -19,6 +21,32 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+    
+    
+def create_poll(request):
+    if request.method == 'POST':
+    	question_text = request.POST['question_text']
+    	choices = [request.POST["1choice"], request.POST["2choice"], request.POST["3choice"],]
+    	
+    	
+    	
+    	#Below is SQl Injection flaw (rows 34-35)
+    	with connection.cursor() as cursor:
+    		cursor.execute(f"INSERT INTO polls_question (question_text, pub_date) VALUES ('{question_text}', '{timezone.now()}')")
+    	# To fix this flaw (row 37)
+    	# Question.objects.create(question_text=question_text, pub_date=timezone.now())
+    
+    
+    
+    
+    	latest_question = Question.objects.latest("id")
+    	for choice in choices:
+    		Choice.objects.create(question=latest_question, choice_text=choice, votes=0)
+    	
+    	return HttpResponseRedirect(reverse("polls:index"))
+    	
+    return HttpResponse("error")
+    
     
 
 def vote(request, question_id):
